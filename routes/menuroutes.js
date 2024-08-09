ba386165c9c929f558544f80fa29b0c12fa74912
Sushant Jadhav <sushantjadhav9112002@gmail.com>
2024-08-09 19:14:03 +0530

    second version of project

--- routes/menuroutes.js
+++ routes/menuroutes.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
         console.log('data saved');
         res.status(200).json(response);
     }
-
+//comment added
     catch (err) {
         console.log(err);
         res.status(500).json({ errror: 'Internal server error.' });
