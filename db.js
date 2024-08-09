4856ced7d66ac2f72865dbf9a08b0bba3eb4da42
Sushant Jadhav <sushantjadhav9112002@gmail.com>
2024-08-09 20:41:43 +0530

    shifted mongodb server to online atlas

--- db.js
+++ db.js
@@ -1,25 +1,27 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongourl = 'mongodb://127.0.0.1:27017/hotels';
+// Connection string with database name included
+// const mongourl = process.env.mongdb_url_local;
+const mongourl = process.env.MONGODB_URL;
 
-mongoose.connect(mongourl,{
-    // useNewUrlParser:true,
-    // useUnifiedTopology:true
-    })
+mongoose.connect(mongourl, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+});
 
+const db = mongoose.connection;
 
-const db=mongoose.connection;
+db.on('connected', () => {
+  console.log('Connected to MongoDB server successfully');
+});
 
-db.on('connected',()=>{
-    console.log('connected to mongodb server successfully');
-})
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
 
-db.on('error',(err)=>{
-    console.error('mongodb connection error',err);
-})
-
-db.on('disconnected',()=>{
-    console.log('Mongodb disconnected.');
-})
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected.');
+});
 
 module.exports = db;
